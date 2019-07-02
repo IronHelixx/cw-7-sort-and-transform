1e947bbee7f833507f3f482ca1ed990a8c90751f
IronHelixx <Davis_Jesse@Yahoo.com>
2019-07-02 15:07:12 -0500

    base solution added - refine later

--- sort-and-transform.js
+++ sort-and-transform.js
@@ -1,3 +1,12 @@
-function sortTransform(a){
+//function sortTransform(a){
+  //
+//}
 
+function sortTransform(a){
+  let first = transform(a);
+  let second = transform(a.sort((a,b) => a - b));
+  let third = transform(a.sort((a,b) => b - a));
+  return `${first}-${second}-${third}-${second}`;
 }
+
+let transform = (arr) => [...arr.slice(0,2), ...arr.slice(arr.length - 2)].map(a => String.fromCharCode(a)).join('');
