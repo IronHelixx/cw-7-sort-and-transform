1f7252fcdad940eed20dac7f92340b1e62f62c42
IronHelixx <Davis_Jesse@Yahoo.com>
2019-07-02 22:18:49 -0500

    google-fu solution

--- sort-and-transform.js
+++ sort-and-transform.js
@@ -1,7 +1,18 @@
-//function sortTransform(a){
-  //
-//}
+function sortTransform(z) {
+  const getW = w =>
+    w
+      .slice(0, 2)
+      .concat(w.slice(-2))
+      .map(i => (typeof i == "number" ? String.fromCharCode(i) : i)).join``;
+  return [
+    getW(z),
+    getW(z.sort((a, b) => a - b)),
+    getW(z.sort((a, b) => b - a)),
+    getW(z.map(i => String.fromCharCode(i)).sort())
+  ].join`-`;
+}
 
+/* **
 function sortTransform(a){
   let first = transform(a);
   let second = transform(a.sort((a,b) => a - b));
@@ -10,3 +21,4 @@ function sortTransform(a){
 }
 
 let transform = (arr) => [...arr.slice(0,2), ...arr.slice(arr.length - 2)].map(a => String.fromCharCode(a)).join('');
+** */
